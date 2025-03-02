41d77e61747dd73e4532d79beb1f7ba3cac244f8
niklaus699 <nicholasbasseyy@gmail.com>
2025-03-02 19:20:20 +0100

    Initial commit

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const validateForm = () => {
 // Name Validation
 const namePattern = /^[a-zA-Z ]+$/;
 const validateFirstName = (firstName) => {
-    if (firstName.value.trim() === "" || firstName.value.length < 3) {
+    if (firstName.value.trim() === "") {
         document.getElementById("firstNameError").style.visibility = "visible";
         firstName.classList.remove("success");
         firstName.classList.add("error");
@@ -56,6 +56,9 @@ const validateFirstName = (firstName) => {
         firstName.classList.remove("success");
         firstName.classList.add("error");
         return false;
+    } else if (firstName.value.length < 3) {
+        handleStyleOf(firstName, idName = "firstNameError", textContent = "Name has to be at least 3 letters");
+        return false
     } else if (namePattern.test(firstName.value) && firstName.value.trim() != "" && firstName.value.length >= 3) {
         document.getElementById("firstNameError").style.visibility = "hidden";
         firstName.classList.remove("error");
@@ -65,7 +68,7 @@ const validateFirstName = (firstName) => {
 }
 
 const validateLastName = (lastName) => {
-    if (lastName.value.trim() === "" || lastName.value.length < 3) {
+    if (lastName.value.trim() === "") {
         document.getElementById("lastNameError").style.visibility = "visible";
         lastName.classList.remove("success");
         lastName.classList.add("error");
@@ -75,6 +78,9 @@ const validateLastName = (lastName) => {
         document.getElementById("lastNameError").style.visibility = "visible";
         lastName.classList.remove("success");
         lastName.classList.add("error");
+        return false;
+    } else if (lastName.value.length < 3) {
+        handleStyleOf(lastName, idName = "lastNameError", textContent = "Name has to be at least 3 letters");
         return false;
     } else if (namePattern.test(lastName.value) && lastName.value.trim() != "" && lastName.value.length >= 3) {
         document.getElementById("lastNameError").style.visibility = "hidden";
@@ -87,10 +93,13 @@ const validateLastName = (lastName) => {
 // Email Validation
 const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const emailValidation = (email) => {
-    if (email.value.trim() === '' || !emailPattern.test(email.value)) {
+    if (email.value.trim() === '') {
         document.getElementById("emailError").style.visibility = "visible";
         email.classList.remove("success");
         email.classList.add("error");
+        return false;
+    } else if (!emailPattern.test(email.value)) {
+        handleStyleOf(email, idName = "emailError", textContent = "Invalid Format For Email Address");
         return false;
     } else if (emailPattern.test(email.value) && !email.value.trim() === '') {
         document.getElementById("emailError").style.visibility = "hidden";
@@ -114,10 +123,13 @@ const queryTypeValidation = () => {
 
 // Message Validation
 const messageValidation = (message) => {
-    if (message.value.trim() === "" || message.value.length < 10) {
+    if (message.value.trim() === "") {
         document.getElementById("messageError").style.visibility = "visible";
         message.classList.remove("success");
         message.classList.add("error");
+        return false;
+    } else if (message.value.length < 10) {
+        handleStyleOf(message, idName = "messageError", textContent = "Message has to contain at least 10 characters");
         return false;
     } else if (message.value.length >= 10 && message.value.trim() != "") {
         document.getElementById("messageError").style.visibility = "hidden";
@@ -136,4 +148,12 @@ const consentValidation = (consent) => {
         document.getElementById("consentError").style.visibility = "hidden";
         return true;
     }
+}
+
+const handleStyleOf = (element, idName, textContent) => {
+    const idVariable = document.getElementById(`${idName}`);
+    idVariable.style.visibility = "visible";
+    idVariable.textContent = `${textContent}`;
+    element.classList.remove("success");
+    element.classList.add("error");
 }
