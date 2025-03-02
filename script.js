72e0f94a7d631bf9b27530533fcd0a8beba82206
niklaus699 <nicholasbasseyy@gmail.com>
2025-03-02 16:55:59 +0100

    Initial commit

--- script.js
+++ script.js
@@ -47,11 +47,13 @@ const namePattern = /^[a-zA-Z ]+$/;
 const validateFirstName = (firstName) => {
     if (firstName.value.trim() === "" || firstName.value.length < 3) {
         document.getElementById("firstNameError").style.visibility = "visible";
+        firstName.classList.remove("success");
         firstName.classList.add("error");
         return false;
     } else if (!namePattern.test(firstName.value)) {
         document.getElementById("firstNameError").textContent = 'Enter a valid name(Only letters Allowed)';
         document.getElementById("firstNameError").style.visibility = "visible";
+        firstName.classList.remove("success");
         firstName.classList.add("error");
         return false;
     } else if (namePattern.test(firstName.value) && firstName.value.trim() != "" && firstName.value.length >= 3) {
@@ -65,11 +67,13 @@ const validateFirstName = (firstName) => {
 const validateLastName = (lastName) => {
     if (lastName.value.trim() === "" || lastName.value.length < 3) {
         document.getElementById("lastNameError").style.visibility = "visible";
+        lastName.classList.remove("success");
         lastName.classList.add("error");
         return false;
     } else if (!namePattern.test(lastName.value)) {
         document.getElementById("lastNameError").textContent = 'Enter a valid name(Only letters Allowed)';
         document.getElementById("lastNameError").style.visibility = "visible";
+        lastName.classList.remove("success");
         lastName.classList.add("error");
         return false;
     } else if (namePattern.test(lastName.value) && lastName.value.trim() != "" && lastName.value.length >= 3) {
@@ -85,6 +89,7 @@ const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const emailValidation = (email) => {
     if (email.value.trim() === '' || !emailPattern.test(email.value)) {
         document.getElementById("emailError").style.visibility = "visible";
+        email.classList.remove("success");
         email.classList.add("error");
         return false;
     } else if (emailPattern.test(email.value) && !email.value.trim() === '') {
@@ -111,6 +116,7 @@ const queryTypeValidation = () => {
 const messageValidation = (message) => {
     if (message.value.trim() === "" || message.value.length < 10) {
         document.getElementById("messageError").style.visibility = "visible";
+        message.classList.remove("success");
         message.classList.add("error");
         return false;
     } else if (message.value.length >= 10 && message.value.trim() != "") {
